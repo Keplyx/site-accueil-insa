106e0b43aa99c1f3da2b818772fd0ec095c3a2ee
Keplyx <keplyx@gmail.com>
2018-05-24 18:05:27 +0200

    Added main page buttons

---
+++ includes/main_button_template.php
@@ -0,0 +1,8 @@
+<a class="main-link" href="<?php echo $link ?>">
+    <span class="main-logo">
+        <i class="<?php echo $icon ?>"></i>
+    </span>
+    <span class="main-text">
+        <span><?php echo $text ?></span>
+    </span>
+</a>
--- index.php
+++ index.php
@@ -23,6 +23,52 @@ include("includes/sidenav.html");
 <div id="home_content_wrap" class="outer">
     <section id="home_content" class="inner">
         <h1>Semaine d'Accueil 2018</h1>
+        <?php
+        $link = "planning.php";
+        $icon = "fas fa-calendar-alt";
+        $text = "Le planning de ta semaine ! Sois au courant de tous les événements !";
+        include("includes/main_button_template.php");
+
+        $link = "parrainage.php";
+        $icon = "fas fa-user-plus";
+        $text = "Toutes les informations concernant le parrainage !";
+        include("includes/main_button_template.php");
+
+        $link = "blouse.php";
+        $icon = "fas fa-tshirt";
+        $text = "La fameuse Blouse, habit officiel de l'insa !";
+        include("includes/main_button_template.php");
+
+        $link = "photos.php";
+        $icon = "fas fa-camera";
+        $text = "Les photos de la semaine (la tienne et celles de tes parrains/marraines) !";
+        include("includes/main_button_template.php");
+
+        $link = "stats.php";
+        $icon = "fas fa-list-ol";
+        $text = "Les stats de chaque groupe (USA vs URSS) !";
+        include("includes/main_button_template.php");
+
+        $link = "coms.php";
+        $icon = "fas fa-users";
+        $text = "Les différentes COM présentes pour assurer la pérénité de la semaine !";
+        include("includes/main_button_template.php");
+
+        $link = "prevs.php";
+        $icon = "fas fa-medkit";
+        $text = "La prévention, pour éviter tout incident";
+        include("includes/main_button_template.php");
+
+        $link = "downloads.php";
+        $icon = "fas fa-download";
+        $text = "Les téléchargements, si t'as paumé une feuille !";
+        include("includes/main_button_template.php");
+
+        $link = "infos.php";
+        $icon = "fas fa-info";
+        $text = "Les informations supplémentaires, si tu veux tout savoir !";
+        include("includes/main_button_template.php");
+        ?>
     </section>
 </div>
 </body>
