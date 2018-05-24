232c8ed5c645474753896715af2330b79b830872
Keplyx <keplyx@gmail.com>
2018-05-24 16:38:25 +0200

    Improved Com layout

--- includes/coms/com_template.php
+++ includes/coms/com_template.php
@@ -1,19 +1,23 @@
 <h3 id="<?= $comId ?>"><?= $comTitle ?></h3>
 <table id="table_coms">
     <tr>
-        <td colspan="2" class="com_description"><?= $comDescription ?></td>
+        <td colspan="4" class="com_description"><?= $comDescription ?></td>
     </tr>
     <tr>
+        <td rowspan="2" class="spacer"></td>
         <td><?= $comRespo ?></td>
-        <td rowspan="2" class="com_photo"><img src="<?= $comRespoPhoto ?>"></td>
+        <td rowspan="2"><img src="<?= $comRespoPhoto ?>"></td>
+        <td rowspan="2" class="spacer"></td>
     </tr>
     <tr>
         <td><span class="fas fa-envelope"></span> Mail : <?= $comRespoId ?></td>
     </tr>
     <?php if ($comRespo2 != ""): ?>
     <tr>
+        <td rowspan="2" class="spacer"></td>
         <td><?= $comRespo2 ?></td>
-        <td rowspan="2" class="com_photo"><img src="<?= $comRespo2Photo ?>"></td>
+        <td rowspan="2"><img src="<?= $comRespo2Photo ?>"></td>
+        <td rowspan="2" class="spacer"></td>
     </tr>
     <tr>
         <td><span class="fas fa-envelope"></span> Mail : <?= $comRespo2Id ?></td>
